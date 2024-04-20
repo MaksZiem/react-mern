98764bf2c8fa9601dc4c415a4988bef30c3c9713
Maks Ziembicki <mtziembicki@gmail.com>
2024-04-20 14:04:28 +0200

    styling user components

---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import UsersList from "../components/UsersList";
+
+export const Users = () => {
+  const USERS = [
+    {
+      id: 1,
+      name: "Max",
+      image:
+        "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      placeCount: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
